5c8d88ecbe61c80e24fd7d90612d5b3ac259c169
Samantha Marshall <me@samdmarshall.com>
2015-11-01 13:17:06 -0500

    [Fix] * adding code to parse nodes of an xcworkspacedata file * added path relative resolution

--- pyxcwsdata/xcworkspacedata.py
+++ pyxcwsdata/xcworkspacedata.py
@@ -1,16 +1,26 @@
 import os
 import xml.etree.ElementTree as xml
 
+from xcworkspacenode import xcworkspacenode
+
 class xcworkspacedata(object):
     
     def __init__(self, wsdata_file_path):
         self.filePath = wsdata_file_path
+        contents = None
         try:
-            self.contents = xml.parse(self.filePath)
+            contents = xml.parse(self.filePath)
         except:
             print('Unable to load contents.xcworkspacedata file!')
             raise
         
-        self.objects = set()
-        for child in self.contents.getroot():
-            print child.tag
+        workspace_base_path = os.path.dirname(os.path.dirname(self.filePath))
+        self.objects = xcworkspacenode.parseNodesWithPath(contents.getroot(), workspace_base_path)
+        
+    def projects(self):
+        project_file_paths = list()
+        for item in self.objects:
+            if item.isFile() and item.filePath.endswith(('.xcodeproj', '.pbproj')):
+                project_file_paths.append(item.filePath)
+        return project_file_paths
+                
